2ab6152c38dede00f65acbea048b2185827841df
Angel <a00531930@gmail.com>
2026-01-13 08:07:23 +0530

    add Decimal_To_Binary function

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ else if(input_val=='hex')
     input_div.innerHTML=`<label for="in_hex">Enter a Hexadecimal Number :</label><br>
                 <input type="text" id="in_hex">`
 }
+else if(input_val=='binary')
+{
+    input_div.innerHTML=` <label for="bin">Enter a Binary Number :</label><br>
+                <input type="text" id="bin">`
+}
 }
 function Get_Output_Val(){
 let output_div=document.getElementById('output')
@@ -46,8 +51,18 @@ else if(output_val=='hex')
 function Convert_function(){
     if(input_val=='binary' && output_val=='decimal')
     {
-      let dec_val=Binary_To_Decimal(input_val)
+      let val=document.getElementById('bin').value 
+      let dec_val=Binary_To_Decimal(val)
       console.log(dec_val)
+      let out_dec=document.getElementById('out_dec')
+      out_dec.value=dec_val.toString()
+    }
+    else if(input_val=='decimal' && output_val=='binary')
+    {
+        let val=document.getElementById('in_dec').value 
+        let bin_val=Decimal_To_Binary(val)
+        let out_bin=document.getElementById('out_bin')
+        out_bin.value=bin_val
     }
 }
 function Binary_To_Decimal(a)
@@ -58,8 +73,25 @@ function Binary_To_Decimal(a)
     for(let i=0;i<len;i++)
     {
      let c=(Math.pow(2,b))*parseInt(a[i])
+     b-=1
      sum+=c
     }
     return sum
 }
+function Decimal_To_Binary(a)
+{
+  let b=parseInt(a)
+  let result=''
+   console.log(b)
+  while(b>0)
+  {
+   let r=b%2
+   result=result+(r.toString())
+    console.log(result)
+   b=parseInt(b/2)
+  }
+  rev_result=result.split('').reverse().join('')
+  return rev_result
+}
+
 
